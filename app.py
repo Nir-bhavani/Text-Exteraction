4dd190426054e239583910789a755f2b515b904b
Nir-bhavani <bhavaninir409@gmail.com>
2025-03-24 13:13:46 +0530

    Update app.py
    git add requirements.txt
    git commit -m "Updated Flask & added Gunicorn"
    git push origin main

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from app import app
+import os  # Import os module
 
 if __name__ == '__main__':
-   app.run()
+    port = int(os.environ.get("PORT", 10000))  # Use Render's dynamic port
+    app.run(host="0.0.0.0", port=port)  # Bind to 0.0.0.0
